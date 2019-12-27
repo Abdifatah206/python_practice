1df05a46a99ca50228cee1ed0643b6f452a3802c
Abdifatah.206 <shire2567@gmail.com>
2019-12-26 21:31:38 -0800

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 21:26:54 2019
+
+@author: Shire
+"""
+
+x =8
+r = x%2
+if r == 0:
+    print('even')
+    if x > 5:
+        print('great')
+    else:
+        print('not so great')
+else:
+    print('odd')
+print('bye')
--- userinput.py
+++ userinput.py
@@ -1,4 +1,6 @@
 x = input('Enter 1st number:')
 y = input('Enter 2nd number:')
-z =x + y
+x = 8
+y = 9
+z = x + y
 print(z)
